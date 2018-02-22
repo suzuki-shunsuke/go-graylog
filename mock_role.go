bce56af275e4b6917470b3314b423630852a8fca
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-02-22 09:31:28 +0900

    refactor: refactor mock_role.go

--- mock_role.go
+++ mock_role.go
@@ -49,32 +49,36 @@ func (ms *MockServer) handleGetRole(w http.ResponseWriter, r *http.Request) {
 
 func (ms *MockServer) handleUpdateRole(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	name := path.Base(r.URL.Path)
-	if name != "Admin" {
-		t := Error{
-			Message: fmt.Sprintf("No role found with name %s", name),
-			Type:    "ApiError"}
-		b, err := json.Marshal(&t)
-		if err != nil {
-			w.Write([]byte(`{"message":"500 Internal Server Error"}`))
-			return
-		}
-		w.Write(b)
-		return
-	}
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
+		w.WriteHeader(500)
 		w.Write([]byte(`{"message":"500 Internal Server Error"}`))
+		return
+	}
+	name := path.Base(r.URL.Path)
+	if _, ok := ms.Roles[name]; !ok {
+		w.WriteHeader(404)
+		w.Write([]byte(fmt.Sprintf(`{"type": "ApiError", "message": "No role found with name %s"}`, name)))
 		return
 	}
 	role := Role{}
 	err = json.Unmarshal(b, &role)
 	if err != nil {
+		w.WriteHeader(400)
 		w.Write([]byte(`{"message":"400 Bad Request"}`))
 		return
 	}
+	sc, msg := validateRole(&role)
+	if sc != 200 {
+		w.WriteHeader(sc)
+		w.Write(msg)
+		return
+	}
+	delete(ms.Roles, name)
+	ms.Roles[role.Name] = role
 	b, err = json.Marshal(&role)
 	if err != nil {
+		w.WriteHeader(500)
 		w.Write([]byte(`{"message":"500 Internal Server Error"}`))
 		return
 	}
@@ -82,6 +86,16 @@ func (ms *MockServer) handleUpdateRole(w http.ResponseWriter, r *http.Request) {
 }
 
 func (ms *MockServer) handleDeleteRole(w http.ResponseWriter, r *http.Request) {}
+
+func validateRole(role *Role) (int, []byte) {
+	if role.Name == "" {
+		return 400, []byte(`{"type": "ApiError", "message": "Can not construct instance of org.graylog2.rest.models.roles.responses.RoleResponse, problem: Null name\n at [Source: org.glassfish.jersey.message.internal.ReaderInterceptorExecutor$UnCloseableInputStream@472db3c8; line: 1, column: 31]"}`)
+	}
+	if role.Permissions == nil || len(role.Permissions) == 0 {
+		return 400, []byte(`{"type": "ApiError", "message": "Can not construct instance of org.graylog2.rest.models.roles.responses.RoleResponse, problem: Null permissions\n at [Source: org.glassfish.jersey.message.internal.ReaderInterceptorExecutor$UnCloseableInputStream@7e64a22d; line: 1, column: 16]"}`)
+	}
+	return 200, []byte("")
+}
 
 func (ms *MockServer) handleCreateRole(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -98,14 +112,10 @@ func (ms *MockServer) handleCreateRole(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`{"message":"400 Bad Request"}`))
 		return
 	}
-	if role.Name == "" {
-		w.WriteHeader(400)
-		w.Write([]byte(`{"type": "ApiError", "message": "Can not construct instance of org.graylog2.rest.models.roles.responses.RoleResponse, problem: Null name\n at [Source: org.glassfish.jersey.message.internal.ReaderInterceptorExecutor$UnCloseableInputStream@472db3c8; line: 1, column: 31]"}`))
-		return
-	}
-	if role.Permissions == nil || len(role.Permissions) == 0 {
-		w.WriteHeader(400)
-		w.Write([]byte(`{"type": "ApiError", "message": "Can not construct instance of org.graylog2.rest.models.roles.responses.RoleResponse, problem: Null permissions\n at [Source: org.glassfish.jersey.message.internal.ReaderInterceptorExecutor$UnCloseableInputStream@7e64a22d; line: 1, column: 16]"}`))
+	sc, msg := validateRole(&role)
+	if sc != 200 {
+		w.WriteHeader(sc)
+		w.Write(msg)
 		return
 	}
 	if _, ok := ms.Roles[role.Name]; ok {
