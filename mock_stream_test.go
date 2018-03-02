a0da661cb825d8074accf257447211f42f13c2f0
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-03-02 18:27:47 +0900

    test: improve test coverage

--- mock_stream_test.go
+++ mock_stream_test.go
@@ -1,24 +1,75 @@
 package graylog
 
 import (
-	"fmt"
+	"bytes"
+	"io"
 	"net/http"
 	"testing"
 )
 
-func TestMockServerHandleUpdateStream(t *testing.T) {
-	server, _, err := getServerAndClient()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer server.Close()
-	endpoint := fmt.Sprintf("%s/dummy", server.Endpoint)
-	req, err := http.NewRequest(http.MethodGet, endpoint, nil)
+func testUpdateStream400(t *testing.T, endpoint string, body io.Reader) {
+	req, err := http.NewRequest(
+		http.MethodPut, endpoint, body)
 	if err != nil {
 		t.Fatal(err)
 	}
 	hc := &http.Client{}
-	if _, err := hc.Do(req); err != nil {
+	resp, err := hc.Do(req)
+	if err != nil {
 		t.Fatal(err)
+	}
+	if resp.StatusCode != 400 {
+		t.Fatalf("resp.StatusCode == %d, wanted 400", resp.StatusCode)
+	}
+}
+
+func TestMockServerHandleUpdateStream(t *testing.T) {
+	server, client, err := getServerAndClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.Close()
+	stream := dummyStream()
+	server.Streams[stream.Id] = *stream
+	endpoint := client.endpoints.Stream(stream.Id)
+
+	body := bytes.NewBuffer([]byte("hoge"))
+	testUpdateStream400(t, endpoint, body)
+
+	body = bytes.NewBuffer([]byte(`{"title": 0}`))
+	testUpdateStream400(t, endpoint, body)
+
+	body = bytes.NewBuffer([]byte(`{"description": 0}`))
+	testUpdateStream400(t, endpoint, body)
+
+	body = bytes.NewBuffer([]byte(`{"matching_type": 0}`))
+	testUpdateStream400(t, endpoint, body)
+
+	body = bytes.NewBuffer([]byte(`{"remove_matches_from_default_stream": 0}`))
+	testUpdateStream400(t, endpoint, body)
+
+	body = bytes.NewBuffer([]byte(`{"index_set_id": 0}`))
+	testUpdateStream400(t, endpoint, body)
+}
+
+func TestMockServerHandleCreateStream(t *testing.T) {
+	server, client, err := getServerAndClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.Close()
+	body := bytes.NewBuffer([]byte("hoge"))
+	req, err := http.NewRequest(
+		http.MethodPost, client.endpoints.Streams, body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	hc := &http.Client{}
+	resp, err := hc.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != 400 {
+		t.Fatalf("resp.StatusCode == %d, wanted 400", resp.StatusCode)
 	}
 }
