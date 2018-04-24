ea7d4dee3af120c540404026b11d8a24d6c8a448
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-04-24 23:08:59 +0900

    ci: fix .travis.yml and lint.sh

--- lint.sh
+++ lint.sh
@@ -4,8 +4,11 @@ echo "+ gofmt"
 ! git ls-files | grep .go | xargs gofmt -s -d | grep '^'
 echo "+ go vet"
 go vet $(go list ./... | grep -v /vendor/)
-echo "+ golint"
-golint client terraform/... mockserver mockserver/store validator
+# https://github.com/golang/lint/issues/397
+if which golint &> /dev/null; then
+  echo "+ golint"
+  golint ./mockserver ./client ./terraform/... ./mockserver/logic ./mockserver/store ./mockserver/store/plain ./mockserver/handler ./validator
+fi
 echo "+ gosimple"
 ! gosimple ./... | grep '^'
 echo "+ unused"
