6cfb4e62e4256366c93713e750c9f991fffef88e
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-04-26 09:34:31 +0900

    ci: use gometalinter

--- lint.sh
+++ lint.sh
@@ -1,17 +1,6 @@
-set -e
-
-echo "+ gofmt"
-! git ls-files | grep .go | xargs gofmt -s -d | grep '^'
-echo "+ go vet"
-go vet $(go list ./... | grep -v /vendor/)
-# https://github.com/golang/lint/issues/397
-if which golint &> /dev/null; then
-  echo "+ golint"
-  golint ./mockserver ./client ./terraform/... ./mockserver/logic ./mockserver/store ./mockserver/store/plain ./mockserver/handler ./validator
-fi
-echo "+ gosimple"
-! gosimple ./... | grep '^'
-echo "+ unused"
-unused ./...
+echo "+ gometalinter"
+npm run metalint || exit 1
+echo "+ golint"
+golint ./mockserver ./client ./terraform/... ./mockserver/logic ./mockserver/store ./mockserver/store/plain ./mockserver/handler ./validator || exit 1
 echo "+ staticcheck (failure is ignored)"
 staticcheck ./... || echo "staticcheck failure is ignored"
