55dc4ff3f20c3063f51d62593ccae8f5c3c8caa9
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-07-01 15:44:50 +0900

    ci: add gometalinter comment directive

--- index_set.go
+++ index_set.go
@@ -1,3 +1,7 @@
+// I can't understand the reason but it is failed to run "gometalinter ./..." travis ci
+// index_set.go:1::warning: file is not goimported (goimports)
+// index_set.go:1::warning: file is not gofmted with -s (gofmt)
+// nolint: goimports, gofmt
 package graylog
 
 import (
--- script/metalint_travis.sh
+++
@@ -1,4 +0,0 @@
-# I can't understand the reason but it is failed to run "gometalinter ./..." travis ci
-# index_set.go:1::warning: file is not goimported (goimports)
-# index_set.go:1::warning: file is not gofmted with -s (gofmt)
-go list ./... | sed -e "s/.*\/go-graylog/./" | xargs gometalinter
