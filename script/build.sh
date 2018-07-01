499277c5ee8e97ce3960c3043676726a439c525b
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-07-01 18:46:13 +0900

    ci: fix script/build.sh

--- script/build.sh
+++ script/build.sh
@@ -3,6 +3,6 @@ echo "pwd: $PWD" || exit 1
 
 source script/decho.sh || exit 1
 
-decho gox -output="dist/${TAG}/graylog-mock-server_${TAG}_{{.OS}}_{{.Arch}}" -osarch="darwin/amd64 linux/amd64 windows/amd64" ./mockserver/exec || exit 1
-decho gox -output="dist/${TAG}/terraform-provider-graylog_${TAG}_{{.OS}}_{{.Arch}}" -osarch="darwin/amd64 linux/amd64 windows/amd64" ./terraform || exit 1
+gox -output="dist/${TAG}/graylog-mock-server_${TAG}_{{.OS}}_{{.Arch}}" -osarch="darwin/amd64 linux/amd64 windows/amd64" ./mockserver/exec || exit 1
+gox -output="dist/${TAG}/terraform-provider-graylog_${TAG}_{{.OS}}_{{.Arch}}" -osarch="darwin/amd64 linux/amd64 windows/amd64" ./terraform/ || exit 1
 ls dist/${TAG} | xargs -I {} gzip dist/${TAG}/{}
