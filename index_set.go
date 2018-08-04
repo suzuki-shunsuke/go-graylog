69c5ec6a9c35515f40f632a2773f476038abf1d8
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-08-04 17:21:26 +0900

    build: remove an invalid comment

--- index_set.go
+++ index_set.go
@@ -1,7 +1,3 @@
-// I can't understand the reason but it is failed to run "gometalinter ./..." travis ci
-// index_set.go:1::warning: file is not goimported (goimports)
-// index_set.go:1::warning: file is not gofmted with -s (gofmt)
-// nolint: goimports, gofmt
 package graylog
 
 import (
