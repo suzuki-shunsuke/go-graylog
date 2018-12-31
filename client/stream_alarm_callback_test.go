3b155d754e6f4ad9560257e61bbf7f70631fa591
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-12-31 16:31:34 +0900

    test: add tests

--- client/stream_alarm_callback_test.go
+++ client/stream_alarm_callback_test.go
@@ -28,7 +28,7 @@ func TestGetStreamAlarmCallbacks(t *testing.T) {
 	}{{
 		statusCode: 200,
 		resp: `{
-  "total": 3,
+  "total": 4,
   "alarmcallbacks": [
     {
       "id": "5c08bb0dc9e77c0000000000",
@@ -81,10 +81,27 @@ func TestGetStreamAlarmCallbacks(t *testing.T) {
       "title": "http alarm callback",
       "created_at": "2018-12-30T08:47:32.865+0000",
       "creator_user_id": "admin"
-    }
+    },
+		{
+	    "id": "5c29bb09df46c60001ab3af3",
+      "type": "org.graylog2.alarmcallbacks.hipchat.HipChatAlarmCallback",
+      "configuration": {
+        "color": "green",
+        "api_url": "https://api.hipchat.com",
+        "message_template": "test template",
+        "api_token": "test",
+        "graylog_base_url": "http://localhost:9000",
+        "notify": true,
+        "room": "test"
+      },
+      "stream_id": "000000000000000000000001",
+      "title": "test",
+      "created_at": "2018-12-31T06:45:29.907+0000",
+      "creator_user_id": "admin"	
+		}
   ]
 }`,
-		total: 3,
+		total: 4,
 		acs: []graylog.AlarmCallback{
 			{
 				ID:            "5c08bb0dc9e77c0000000000",
@@ -127,6 +144,24 @@ func TestGetStreamAlarmCallbacks(t *testing.T) {
 				StreamID:      "5b93b425c9e0000000000000",
 				Title:         "http alarm callback",
 				CreatedAt:     "2018-12-30T08:47:32.865+0000",
+				CreatorUserID: "admin",
+			}, {
+				ID: "5c29bb09df46c60001ab3af3",
+				Configuration: &graylog.GeneralAlarmCallbackConfiguration{
+					Type: "org.graylog2.alarmcallbacks.hipchat.HipChatAlarmCallback",
+					Configuration: map[string]interface{}{
+						"color":            "green",
+						"api_url":          "https://api.hipchat.com",
+						"message_template": "test template",
+						"api_token":        "test",
+						"graylog_base_url": "http://localhost:9000",
+						"notify":           true,
+						"room":             "test",
+					},
+				},
+				StreamID:      "000000000000000000000001",
+				Title:         "test",
+				CreatedAt:     "2018-12-31T06:45:29.907+0000",
 				CreatorUserID: "admin",
 			},
 		},
@@ -211,33 +246,111 @@ func TestCreateStreamAlarmCallback(t *testing.T) {
 		statusCode: 201,
 		acID:       "d3ecb503-b767-4d59-bf6a-e2c000000000",
 		req: map[string]interface{}{
-			"type":          "org.graylog2.alarmcallbacks.hipchat.HipChatAlarmCallback",
-			"title":         "hipchat alarm callback",
+			"type":  "org.graylog2.alarmcallbacks.hipchat.HipChatAlarmCallback",
+			"title": "hipchat alarm callback",
 			"configuration": map[string]interface{}{
-				//				"color":            "yellow",
-				//				"api_url":          "https://api.hipchat.com",
-				//				"message_template": "test template",
-				//				"api_token":        "test",
-				//				"graylog_base_url": "http://localhost:9000",
-				//				"room":             "test",
-				//				"notify":           true,
+				"color":            "yellow",
+				"api_url":          "https://api.hipchat.com",
+				"message_template": "test template",
+				"api_token":        "test",
+				"graylog_base_url": "http://localhost:9000",
+				"room":             "test",
+				"notify":           true,
 			},
 		},
 		ac: graylog.AlarmCallback{
 			Configuration: &graylog.GeneralAlarmCallbackConfiguration{
-				Type:          "org.graylog2.alarmcallbacks.hipchat.HipChatAlarmCallback",
+				Type: "org.graylog2.alarmcallbacks.hipchat.HipChatAlarmCallback",
 				Configuration: map[string]interface{}{
-					//					"color":            "yellow",
-					//					"api_url":          "https://api.hipchat.com",
-					//					"message_template": "test template",
-					//					"api_token":        "test",
-					//					"graylog_base_url": "http://localhost:9000",
-					//					"room":             "test",
-					//					"notify":           true,
+					"color":            "yellow",
+					"api_url":          "https://api.hipchat.com",
+					"message_template": "test template",
+					"api_token":        "test",
+					"graylog_base_url": "http://localhost:9000",
+					"room":             "test",
+					"notify":           true,
 				},
 			},
 			StreamID: "5b93b425c9e0000000000000",
 			Title:    "hipchat alarm callback",
+		},
+		checkErr: require.Nil,
+	}, {
+		statusCode: 201,
+		acID:       "d3ecb503-b767-4d59-bf6a-e2c000000000",
+		req: map[string]interface{}{
+			"type":  graylog.SlackAlarmCallbackType,
+			"title": "slack alarm callback",
+			"configuration": map[string]interface{}{
+				"graylog2_url":   "https://graylog.example.com",
+				"link_names":     true,
+				"color":          "#FF0000",
+				"webhook_url":    "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX",
+				"user_name":      "Graylog",
+				"backlog_items":  5,
+				"channel":        "#general",
+				"custom_message": "${alert_acition.title}\n\n${foreach backlog message}\n<https://graylog.example.com/streams/${stream.id}/search?rangetype=absolute&from=${message.timestamp}&to=${message.timestamp} | link> ${message.message}\n${end}",
+				"notify_channel": false,
+			},
+		},
+		ac: graylog.AlarmCallback{
+			StreamID: "5b93b425c9e0000000000000",
+			Title:    "slack alarm callback",
+			Configuration: &graylog.SlackAlarmCallbackConfiguration{
+				Graylog2URL:   "https://graylog.example.com",
+				LinkNames:     true,
+				Color:         "#FF0000",
+				WebhookURL:    "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX",
+				UserName:      "Graylog",
+				BacklogItems:  5,
+				Channel:       "#general",
+				CustomMessage: "${alert_acition.title}\n\n${foreach backlog message}\n<https://graylog.example.com/streams/${stream.id}/search?rangetype=absolute&from=${message.timestamp}&to=${message.timestamp} | link> ${message.message}\n${end}",
+				NotifyChannel: false,
+			},
+		},
+		checkErr: require.Nil,
+	}, {
+		statusCode: 201,
+		acID:       "d3ecb503-b767-4d59-bf6a-e2c000000000",
+		req: map[string]interface{}{
+			"type":  graylog.EmailAlarmCallbackType,
+			"title": "email alarm callback",
+			"configuration": map[string]interface{}{
+				"user_receivers":  []string{"example"},
+				"body":            "##########\nAlert Description: ${check_result.resultDescription}\nDate: ${check_result.triggeredAt}\nStream ID: ${stream.id}\nStream title: ${stream.title}\nStream description: ${stream.description}\nAlert Condition Title: ${alertCondition.title}\n${if stream_url}Stream URL: ${stream_url}${end}\n\nTriggered acition: ${check_result.triggeredCondition}\n##########\n\n${if backlog}Last messages accounting for this alert:\n${foreach backlog message}${message}\n\n${end}${else}<No backlog>\n${end}\n",
+				"sender":          "graylog@example.org",
+				"subject":         "Graylog alert for stream: ${stream.title}: ${check_result.resultDescription}",
+				"email_receivers": []string{"graylog@example.com"},
+			},
+		},
+		ac: graylog.AlarmCallback{
+			StreamID: "5b93b425c9e0000000000000",
+			Title:    "email alarm callback",
+			Configuration: &graylog.EmailAlarmCallbackConfiguration{
+				UserReceivers:  set.NewStrSet("example"),
+				Body:           "##########\nAlert Description: ${check_result.resultDescription}\nDate: ${check_result.triggeredAt}\nStream ID: ${stream.id}\nStream title: ${stream.title}\nStream description: ${stream.description}\nAlert Condition Title: ${alertCondition.title}\n${if stream_url}Stream URL: ${stream_url}${end}\n\nTriggered acition: ${check_result.triggeredCondition}\n##########\n\n${if backlog}Last messages accounting for this alert:\n${foreach backlog message}${message}\n\n${end}${else}<No backlog>\n${end}\n",
+				Sender:         "graylog@example.org",
+				Subject:        "Graylog alert for stream: ${stream.title}: ${check_result.resultDescription}",
+				EmailReceivers: set.NewStrSet("graylog@example.com"),
+			},
+		},
+		checkErr: require.Nil,
+	}, {
+		statusCode: 201,
+		acID:       "d3ecb503-b767-4d59-bf6a-e2c000000000",
+		req: map[string]interface{}{
+			"type":  graylog.HTTPAlarmCallbackType,
+			"title": "http alarm callback",
+			"configuration": map[string]interface{}{
+				"url": "https://example.com",
+			},
+		},
+		ac: graylog.AlarmCallback{
+			StreamID: "5b93b425c9e0000000000000",
+			Title:    "http alarm callback",
+			Configuration: &graylog.HTTPAlarmCallbackConfiguration{
+				URL: "https://example.com",
+			},
 		},
 		checkErr: require.Nil,
 	}}
