cea5ca7136f2b954dc076b7bbd139e26b3326ec2
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2019-02-27 20:56:43 +0900

    feat: #68 support terraform pipeline_rule resource

--- pipeline_rule.go
+++ pipeline_rule.go
@@ -4,8 +4,9 @@ package graylog
 // http://docs.graylog.org/en/3.0/pages/pipelines/rules.html
 type PipelineRule struct {
 	// required
-	Source      string `json:"source,omitempty" v-create:"required" v-update:"required"`
-	ID          string `json:"id,omitempty" v-create:"isdefault" v-update:"required"`
+	Source string `json:"source,omitempty" v-create:"required" v-update:"required"`
+	ID     string `json:"id,omitempty" v-create:"isdefault" v-update:"required"`
+	// Note that title is ignored in create and update API.
 	Title       string `json:"title,omitempty"`
 	Description string `json:"description,omitempty"`
 	// CreatedAt   string `json:"created_at,omitempty"`
