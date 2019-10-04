21ea8650ab3b4e31378f74b24c382abbab2566d8
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2019-10-04 22:45:41 +0900

    build: fix durl.sh

--- scripts/durl.sh
+++ scripts/durl.sh
@@ -2,6 +2,7 @@ find . \
   -type d -name node_modules -prune -o \
   -type d -name .git -prune -o \
   -type d -name vendor -prune -o \
+  -type d -name dist -prune -o \
   -type f -print | \
   grep -v package-lock.json | \
   grep -v Gopkg.toml | \
