f29cf8b88e91d4e0c459d9dd3adcb36c93f8b68b
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2020-01-13 16:05:44 +0900

    test: add tests of graylog_stream_rule

---
+++ testdata/stream_rule/create.tf
@@ -0,0 +1,8 @@
+resource "graylog_stream_rule" "test" {
+  field       = "tag"
+  value       = "4"
+  stream_id   = "5d84c1a92ab79c000d35d6ca"
+  description = "test"
+  type        = 1
+  inverted    = false
+}
