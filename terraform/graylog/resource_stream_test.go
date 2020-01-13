3e2b2fdc82fe710d17963ae7d779a38aab8affee
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2020-01-13 15:42:54 +0900

    test: add tests of graylog_stream

--- terraform/graylog/resource_stream_test.go
+++ terraform/graylog/resource_stream_test.go
@@ -1,119 +1,62 @@
 package graylog
 
 import (
+	"net/http"
 	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/stretchr/testify/require"
+	"github.com/suzuki-shunsuke/flute/flute"
+
+	"github.com/suzuki-shunsuke/go-graylog/v9/testdata"
 )
 
-// func testDeleteStream(
-// 	ctx context.Context, cl *client.Client, key string,
-// ) resource.TestCheckFunc {
-// 	return func(tfState *terraform.State) error {
-// 		id, err := getIDFromTfState(tfState, key)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		if _, _, err := cl.GetStream(ctx, id); err == nil {
-// 			return fmt.Errorf(`stream "%s" must be deleted`, id)
-// 		}
-// 		return nil
-// 	}
-// }
-
-// func testCreateStream(
-// 	ctx context.Context, cl *client.Client, key string,
-// ) resource.TestCheckFunc {
-// 	return func(tfState *terraform.State) error {
-// 		id, err := getIDFromTfState(tfState, key)
-// 		if err != nil {
-// 			return err
-// 		}
-//
-// 		_, _, err = cl.GetStream(ctx, id)
-// 		return err
-// 	}
-// }
-
-// func testUpdateStream(
-// 	ctx context.Context, cl *client.Client, key, title string,
-// ) resource.TestCheckFunc {
-// 	return func(tfState *terraform.State) error {
-// 		id, err := getIDFromTfState(tfState, key)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		stream, _, err := cl.GetStream(ctx, id)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		if stream.Title != title {
-// 			return fmt.Errorf("stream.Title == %s, wanted %s", stream.Title, title)
-// 		}
-// 		return nil
-// 	}
-// }
-
 func TestAccStream(t *testing.T) {
-	//	ctx := context.Background()
-	//	cl, err := setEnv()
-	//	if err != nil {
-	//		t.Fatal(err)
-	//	}
-	//
-	//	testAccProvider := Provider()
-	//	testAccProviders := map[string]terraform.ResourceProvider{
-	//		"graylog": testAccProvider,
-	//	}
-	//
-	//	u, err := uuid.NewV4()
-	//	if err != nil {
-	//		t.Fatal(err)
-	//	}
-	//	prefix := u.String()
-	//	roleTf := `
-	//resource "graylog_index_set" "test" {
-	//  title = "terraform test index set"
-	//	description = "terraform test index set description"
-	//	index_prefix = "%s"
-	//	shards = 4
-	//	replicas = 0
-	//  rotation_strategy_class = "org.graylog2.indexer.rotation.strategies.MessageCountRotationStrategy"
-	//  rotation_strategy {
-	//    type = "org.graylog2.indexer.rotation.strategies.MessageCountRotationStrategyConfig"
-	//  }
-	//  retention_strategy_class = "org.graylog2.indexer.retention.strategies.DeletionRetentionStrategy"
-	//  retention_strategy {
-	//    type = "org.graylog2.indexer.retention.strategies.DeletionRetentionStrategyConfig"
-	//  }
-	//  index_analyzer = "standard"
-	//	writable = true
-	//  index_optimization_max_num_segments = 1
-	//}
-	//
-	//resource "graylog_stream" "test" {
-	//  title = "%s"
-	//	index_set_id = "${graylog_index_set.test.id}"
-	//	matching_type = "AND"
-	//}`
-	//	createTitle := "terraform stream test"
-	//	updateTitle := "terraform stream test updated"
-	//
-	//	key := "graylog_stream.test"
-	//	resource.Test(t, resource.TestCase{
-	//		Providers:    testAccProviders,
-	//		CheckDestroy: testDeleteStream(ctx, cl, key),
-	//		Steps: []resource.TestStep{
-	//			{
-	//				Config: fmt.Sprintf(roleTf, prefix, createTitle),
-	//				Check: resource.ComposeTestCheckFunc(
-	//					testCreateStream(ctx, cl, key),
-	//				),
-	//			},
-	//			{
-	//				Config: fmt.Sprintf(roleTf, prefix, updateTitle),
-	//				Check: resource.ComposeTestCheckFunc(
-	//					testUpdateStream(ctx, cl, key, updateTitle),
-	//				),
-	//			},
-	//		},
-	//	})
+	setEnv()
+
+	stream := testdata.Stream()
+
+	tc := &testCase{
+		t:          t,
+		Name:       "stream",
+		CreatePath: "/api/streams",
+		GetPath:    "/api/streams/" + stream.ID,
+
+		CreateReqBodyMap:   testdata.CreateStreamReqBodyMap(),
+		UpdateReqBodyMap:   testdata.UpdateStreamReqBodyMap(),
+		CreatedDataPath:    "stream/stream.json",
+		UpdatedDataPath:    "stream/updated_stream.json",
+		CreateRespBodyPath: "stream/create_response.json",
+		UpdateRespBodyPath: "stream/stream.json",
+		CreateTFPath:       "stream/create.tf",
+		UpdateTFPath:       "stream/update.tf",
+	}
+
+	defaultTransport := http.DefaultClient.Transport
+	defer func() {
+		http.DefaultClient.Transport = defaultTransport
+	}()
+	transport, testCase, err := tc.Get()
+	require.Nil(tc.t, err)
+	tp := transport.(*flute.Transport)
+	route := tp.Services[0].Routes[1]
+	route.Matcher.Path = route.Tester.Path
+	tp.Services[0].Routes[1] = route
+	tp.Services[0].Routes = append(tp.Services[0].Routes, flute.Route{
+		Name: "Resume stream",
+		Matcher: &flute.Matcher{
+			Method: "POST",
+			Path:   tc.GetPath + "/resume",
+		},
+		Tester: &flute.Tester{
+			PartOfHeader: getTestHeader(),
+		},
+		Response: &flute.Response{
+			Base: http.Response{
+				StatusCode: 204,
+			},
+		},
+	})
+	http.DefaultClient.Transport = transport
+	resource.Test(tc.t, testCase)
 }
