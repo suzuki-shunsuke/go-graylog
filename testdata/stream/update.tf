381b99f4ad8dff0c000d649976c18d622b43ac5f
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-01-17 21:45:01 +0900

    test: add test of graylog_stream

--- testdata/stream/update.tf
+++ testdata/stream/update.tf
@@ -3,7 +3,7 @@ resource "graylog_stream" "test" {
   description = "updated description"
 
   index_set_id                       = "5d84bfbe2ab79c000d35d4a9"
-  disabled                           = false
+  disabled                           = true
   matching_type                      = "AND"
   remove_matches_from_default_stream = true
 }
