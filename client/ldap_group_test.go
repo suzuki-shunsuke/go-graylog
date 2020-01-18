14b5963f1979f5763d80aa46cc401f6836ffd3d2
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2020-01-18 12:21:49 +0900

    test: replace gock to flute

--- client/ldap_group_test.go
+++ client/ldap_group_test.go
@@ -2,112 +2,159 @@ package client_test
 
 import (
 	"context"
-	"os"
+	"net/http"
 	"testing"
-
-	"gopkg.in/h2non/gock.v1"
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/suzuki-shunsuke/flute/flute"
 	"github.com/suzuki-shunsuke/go-graylog/v9/client"
 )
 
 func TestClient_GetLDAPGroups(t *testing.T) {
 	ctx := context.Background()
-	authName := os.Getenv("GRAYLOG_AUTH_NAME")
-	authPass := os.Getenv("GRAYLOG_AUTH_PASSWORD")
-	endpoint := os.Getenv("GRAYLOG_WEB_ENDPOINT_URI")
+	cl, err := client.NewClient("http://example.com/api", "admin", "admin")
+	require.Nil(t, err)
 
-	if endpoint == "" {
-		defer gock.Off()
-		endpoint = "http://example.com/api"
-		client, err := client.NewClient(endpoint, authName, authPass)
-		require.Nil(t, err)
-		data := []struct {
-			statusCode int
-			resp       string
-			groups     []string
-			checkErr   func(require.TestingT, interface{}, ...interface{})
-		}{{
-			statusCode: 200,
-			resp:       `["foo"]`,
-			groups:     []string{"foo"},
-			checkErr:   require.Nil,
-		}}
-		for _, d := range data {
-			gock.New("http://example.com").
-				Get("/api/system/ldap/groups").
-				MatchType("json").Reply(d.statusCode).BodyString(d.resp)
-			m, _, err := client.GetLDAPGroups(ctx)
-			if err != nil {
-				require.Equal(t, d.groups, m)
-			}
-			d.checkErr(t, err)
+	data := []struct {
+		statusCode int
+		resp       string
+		groups     []string
+		checkErr   func(require.TestingT, interface{}, ...interface{})
+	}{{
+		statusCode: 200,
+		resp:       `["foo"]`,
+		groups:     []string{"foo"},
+		checkErr:   require.Nil,
+	}}
+	for _, d := range data {
+		cl.SetHTTPClient(&http.Client{
+			Transport: &flute.Transport{
+				T: t,
+				Services: []flute.Service{
+					{
+						Endpoint: "http://example.com",
+						Routes: []flute.Route{
+							{
+								Tester: &flute.Tester{
+									Method:       "GET",
+									Path:         "/api/system/ldap/groups",
+									PartOfHeader: getTestHeader(),
+								},
+								Response: &flute.Response{
+									Base: http.Response{
+										StatusCode: d.statusCode,
+									},
+									BodyString: d.resp,
+								},
+							},
+						},
+					},
+				},
+			},
+		})
+
+		m, _, err := cl.GetLDAPGroups(ctx)
+		if err != nil {
+			require.Equal(t, d.groups, m)
 		}
+		d.checkErr(t, err)
 	}
 }
 
 func TestClient_GetLDAPGroupRoleMapping(t *testing.T) {
 	ctx := context.Background()
-	authName := os.Getenv("GRAYLOG_AUTH_NAME")
-	authPass := os.Getenv("GRAYLOG_AUTH_PASSWORD")
-	endpoint := os.Getenv("GRAYLOG_WEB_ENDPOINT_URI")
+	cl, err := client.NewClient("http://example.com/api", "admin", "admin")
+	require.Nil(t, err)
 
-	if endpoint == "" {
-		defer gock.Off()
-		endpoint = "http://example.com/api"
-		client, err := client.NewClient(endpoint, authName, authPass)
-		require.Nil(t, err)
-		data := []struct {
-			statusCode int
-			resp       string
-			checkErr   func(require.TestingT, interface{}, ...interface{})
-		}{{
-			statusCode: 200,
-			resp:       `{"foo": "Reader"}`,
-			checkErr:   require.Nil,
-		}}
-		for _, d := range data {
-			gock.New("http://example.com").
-				Get("/api/system/ldap/settings/groups").
-				MatchType("json").Reply(d.statusCode).BodyString(d.resp)
-			m, _, err := client.GetLDAPGroupRoleMapping(ctx)
-			if err != nil {
-				require.Equal(t, d.resp, m)
-			}
-			d.checkErr(t, err)
+	data := []struct {
+		statusCode int
+		resp       string
+		checkErr   func(require.TestingT, interface{}, ...interface{})
+	}{{
+		statusCode: 200,
+		resp:       `{"foo": "Reader"}`,
+		checkErr:   require.Nil,
+	}}
+	for _, d := range data {
+		cl.SetHTTPClient(&http.Client{
+			Transport: &flute.Transport{
+				T: t,
+				Services: []flute.Service{
+					{
+						Endpoint: "http://example.com",
+						Routes: []flute.Route{
+							{
+								Tester: &flute.Tester{
+									Method:       "GET",
+									Path:         "/api/system/ldap/settings/groups",
+									PartOfHeader: getTestHeader(),
+								},
+								Response: &flute.Response{
+									Base: http.Response{
+										StatusCode: d.statusCode,
+									},
+									BodyString: d.resp,
+								},
+							},
+						},
+					},
+				},
+			},
+		})
+
+		m, _, err := cl.GetLDAPGroupRoleMapping(ctx)
+		if err != nil {
+			require.Equal(t, d.resp, m)
 		}
+		d.checkErr(t, err)
 	}
 }
 
 func TestClient_UpdateLDAPGroupRoleMapping(t *testing.T) {
 	ctx := context.Background()
-	authName := os.Getenv("GRAYLOG_AUTH_NAME")
-	authPass := os.Getenv("GRAYLOG_AUTH_PASSWORD")
-	endpoint := os.Getenv("GRAYLOG_WEB_ENDPOINT_URI")
+	cl, err := client.NewClient("http://example.com/api", "admin", "admin")
+	require.Nil(t, err)
 
-	if endpoint == "" {
-		defer gock.Off()
-		endpoint = "http://example.com/api"
-		client, err := client.NewClient(endpoint, authName, authPass)
-		require.Nil(t, err)
-		data := []struct {
-			statusCode int
-			body       string
-			mapping    map[string]string
-			checkErr   func(require.TestingT, interface{}, ...interface{})
-		}{{
-			statusCode: 204,
-			body:       `{"foo": "Reader"}`,
-			mapping:    map[string]string{"foo": "Reader"},
-			checkErr:   require.Nil,
-		}}
-		for _, d := range data {
-			gock.New("http://example.com").
-				Put("/api/system/ldap/settings/groups").
-				MatchType("json").BodyString(d.body).Reply(d.statusCode)
-			_, err := client.UpdateLDAPGroupRoleMapping(ctx, d.mapping)
-			d.checkErr(t, err)
-		}
+	data := []struct {
+		statusCode int
+		body       string
+		mapping    map[string]string
+		checkErr   func(require.TestingT, interface{}, ...interface{})
+	}{{
+		statusCode: 204,
+		body:       `{"foo": "Reader"}`,
+		mapping:    map[string]string{"foo": "Reader"},
+		checkErr:   require.Nil,
+	}}
+	for _, d := range data {
+		cl.SetHTTPClient(&http.Client{
+			Transport: &flute.Transport{
+				T: t,
+				Services: []flute.Service{
+					{
+						Endpoint: "http://example.com",
+						Routes: []flute.Route{
+							{
+								Tester: &flute.Tester{
+									Method:         "PUT",
+									Path:           "/api/system/ldap/settings/groups",
+									PartOfHeader:   getTestHeader(),
+									BodyJSONString: d.body,
+								},
+								Response: &flute.Response{
+									Base: http.Response{
+										StatusCode: d.statusCode,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		})
+
+		_, err := cl.UpdateLDAPGroupRoleMapping(ctx, d.mapping)
+		d.checkErr(t, err)
 	}
 }
