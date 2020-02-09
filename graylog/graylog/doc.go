94bc85abdef5d256eb3887046e99584723a1dc3f
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2020-02-09 16:07:16 +0900

    docs: fix package document

--- graylog/graylog/doc.go
+++ graylog/graylog/doc.go
@@ -1,7 +1,4 @@
 /*
 Package graylog provides Golang's structs which represents Graylog resource such as roles and users.
-Client and mock server and terraform provider are provided as subpackages.
-
-https://godoc.org/github.com/suzuki-shunsuke/go-graylog/#pkg-subdirectories
 */
 package graylog
