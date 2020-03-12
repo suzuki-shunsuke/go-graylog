096f5c00b3a67cc16990743725dcb704342064d7
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-03-12 20:49:21 +0900

    build: fix install_local_terraform.sh

--- scripts/install_local_terraform.sh
+++ scripts/install_local_terraform.sh
@@ -11,4 +11,4 @@ ee() {
 cd "$(dirname "$0")/.."
 
 ee mkdir -p ~/.terraform.d/plugins
-ee go build -o ~/.terraform.d/plugins/terraform-provider-graylog ./terraform
+ee go build -o ~/.terraform.d/plugins/terraform-provider-graylog ./cmd/terraform-provider-graylog
