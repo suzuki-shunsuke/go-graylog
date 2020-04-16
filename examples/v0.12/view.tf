b5da60b6edc00f15b6166049823088c2a048727f
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-04-16 20:45:10 +0900

    docs: move an example

---
+++ examples/v0.12/view.tf
@@ -0,0 +1,12 @@
+resource "graylog_view" "test" {
+  title       = "test"
+  description = "description"
+  summary     = "summary"
+  # set appropriate search_id
+  # search_id = "5d9529b275d97f58f9539279"
+  # state = {
+  #   "6971d00a-e605-43fb-b873-e4bca773d286" = {
+  #     selected_fields = ["source", "message"]
+  #   }
+  # }
+}
